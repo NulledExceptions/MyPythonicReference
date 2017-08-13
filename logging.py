efe6ff910b868c6f44c1f65f02d19737ce5083ee
Mark Marnell <mark@markmarnell.io>
2017-08-12 21:45:09 -0500

    Update logging.py

--- logging.py
+++ logging.py
@@ -4,6 +4,4 @@ import datetime
 today = datetime.date.today()
 log_name = 'WikiSearch_{}.log'.format(today)
 logging.basicConfig(filename=log_name, level=logging.DEBUG)
-#log = logging.getLogger('econtext')
-#log.addHandler(logging.StreamHandler())
 log.setLevel(logging.DEBUG)
