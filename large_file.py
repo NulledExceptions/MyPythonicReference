42b561210da58b29fc1fe11071845aa91d8de26d
Mark Marnell <mark@markmarnell.io>
2017-08-31 15:29:36 -0500

    Update large_file.py

--- large_file.py
+++ large_file.py
@@ -1,7 +1,8 @@
 import os
 
 cwd = os.getcwd()  
-files = [f.endswith('.txt') for f in os.listdir(cwd)]
+files = ['{}{}'.format(dir_path,f) for f in os.listdir(dir_path) if f.endswith('.json')]
+#files = [f.endswith('.txt') for f in os.listdir(cwd)]
 for file in files:
 	with open(file) as FileObj:
 	    for line in FileObj:
