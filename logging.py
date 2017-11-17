e57d04da468376c6e7d950de0a3f5c03db897d40
Mark Marnell <mark@markmarnell.io>
2017-11-16 21:43:12 -0600

    adding logging locals

--- logging.py
+++ logging.py
@@ -5,3 +5,6 @@ today = datetime.date.today()
 log_name = 'WikiSearch_{}.log'.format(today)
 logging.basicConfig(filename=log_name, level=logging.DEBUG)
 log.setLevel(logging.DEBUG)
+
+
+logger.debug('%r', locals())
